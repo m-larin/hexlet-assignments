326f17e244f819a7156ed53fcbb538def165a197
@hexlet/cli <support@hexlet.io>
2022-11-11 15:35:49 +0300

    submit ruby-basics-ru/testing

---
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/stack'
+
+class StackTest < Minitest::Test
+  # BEGIN
+  def test_stack
+    stack = Stack.new
+    assert { stack.to_a.empty? }
+    assert { stack.empty? }
+    assert { stack.size.zero? }
+
+    stack.push! 'ruby'
+    stack.push! 'php'
+    stack.push! 'java'
+    assert { stack.to_a == %w[ruby php java] }
+    assert { stack.size == 3 }
+    assert { !stack.empty? }
+
+    stack.pop!
+    assert { stack.to_a == %w[ruby php] }
+    assert { stack.size == 2 }
+
+    stack.clear!
+    assert { stack.to_a.empty? }
+    assert { stack.empty? }
+  end
+  # END
+end
+
+test_methods = StackTest.new({}).methods.select { |method| method.to_s.start_with? 'test_' }
+raise 'StackTest has not tests!' if test_methods.empty?
