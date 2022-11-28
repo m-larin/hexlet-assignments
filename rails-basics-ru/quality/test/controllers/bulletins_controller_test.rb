2c299d46122c5ad2ec4a98e19e9321d7dea4188e
@hexlet/cli <support@hexlet.io>
2022-11-28 18:33:49 +0300

    submit rails-basics-ru/quality

---
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class BulletinsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @bulletin = bulletins(:one)
+  end
+
+  test 'should get index' do
+    get bulletins_url
+
+    assert_response :success
+  end
+
+  test 'should show article' do
+    get bulletin_url @bulletin
+
+    assert_response :success
+  end
+end
